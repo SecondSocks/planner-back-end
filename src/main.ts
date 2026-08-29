7191620cce5d2748de75791b54dbb17932b9ce8a
SecondSocks <hitrovalubov267@gmail.com>
2026-08-29 12:24:19 +0700

    Fix: add front url in cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 	app.setGlobalPrefix('api')
 	app.use(cookieParser())
 	app.enableCors({
-		origin: ['http://localhost:3000', 'http://26.197.81.113:3000'],
+		origin: ['http://localhost:3000', 'http://26.197.81.113:3000', 'https://planner-front-end-theta.vercel.app'],
 		credentials: true,
 		exposedHeaders: 'set-cookie'
 	})
